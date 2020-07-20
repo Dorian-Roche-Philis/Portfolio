974eb088598a59c9cb097871ad6e3e91b3eb6406
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-07-21 00:53:28 +0200

    kk

--- app/javascript/components/changed.js
+++ app/javascript/components/changed.js
@@ -1,5 +1,6 @@
 const projectChange = () => {
   let buttons = document.querySelectorAll('btn');
+  console.log(buttons);
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       let slider = document.querySelector('.active > .slidy');
